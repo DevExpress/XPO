a3e96a73b4361293fb33df7b8963ff91cdd1630c
Vyacheslav Donchak <vyacheslav.donchak@devexpress.com>
2017-10-30 17:07:05 +0300

    Minor fixes

--- XpoASPNETCoreDemo/DevExpress.Xpo.AspNetCoreDemo/Program.cs
+++ XpoASPNETCoreDemo/DevExpress.Xpo.AspNetCoreDemo/Program.cs
@@ -19,6 +19,7 @@ namespace DevExpress.Xpo.Demo
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>()
                 .Build();
     }
