8b6fa051a3baf5027cb15cc7a2b5c7566b0c507f
karpenko.maksim <maksim.karpenko@devexpress.com>
2018-04-24 15:40:36 +0300

    Add Web API controller for XPO Profiler logs retrieving

---
+++ XpoASPNETCoreDemo/DevExpress.Xpo.AspNetCoreDemo/XpoLoggerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevExpress.Xpo.Logger;
+
+// This controller implements the methods necessary to enable application profiling with XPO Profiler
+// If you do not plan to use the XPO profiler with this application, remove this class
+
+namespace DevExpress.Xpo.Demo
+{
+    public class XpoLoggerController : Controller
+    {
+        readonly static LoggerBase logger = new LoggerBase(50000);
+        static XpoLoggerController() {
+            LogManager.SetTransport(logger);
+        }
+        [HttpGet]
+        public LogMessage[] GetCompleteLog() {
+            return logger.GetCompleteLog();
+        }
+        [HttpGet]
+        public LogMessage GetMessage() {
+            return logger.GetMessage();
+        }
+        [HttpGet]
+        public LogMessage[] GetMessages(int messagesAmount) {
+            return logger.GetMessages(messagesAmount);
+        }
+    }
+}
