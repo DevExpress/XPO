c3404608895c72969d6c262470dffc11c4c8b664
karpenko.maksim <maksim.karpenko@devexpress.com>
2018-04-24 16:22:35 +0300

    Add XPO ASP.NET MVC Core demo

---
+++ XpoASPNETMVCCoreDemo/DevExpress.Xpo.AspNetMvcCoreDemo/Controllers/DataController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DevExpress.Xpo.AspNetMvcCoreDemo.Models;
+using DevExpress.Xpo.Demo.Entities;
+
+namespace DevExpress.Xpo.AspNetMvcCoreDemo.Controllers
+{
+    public class DataController : Controller
+    {
+        readonly UnitOfWork uow;
+
+        public DataController(UnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public IActionResult Index()
+        {
+            DataViewModel viewModel = new DataViewModel(uow);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id) 
+        {
+            var user = uow.GetObjectByKey<User>(id);
+            if(user != null) {
+                uow.Delete(user);
+                uow.CommitChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel model)
+        {
+            if(model != null) {
+                var newUser = new User(uow) {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email
+                };
+                uow.CommitChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ XpoASPNETMVCCoreDemo/DevExpress.Xpo.AspNetMvcCoreDemo/Core/XpoDemoDataHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Xpo.Demo.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevExpress.Xpo.Demo.Core
+{
+    public static class XpoDemoDataHelper {
+        static readonly string[][] demoData = new string[][]{
+            new string[] { "Amelia", "Harper", "ameliah@dx-email.com" },
+            new string[] { "Lincoln", "Bartlett", "lincolnb@dx-email.com" },
+            new string[] { "Samantha", "Bright", "samanthab@dx-email.com" },
+            new string[] { "David", "Jones", "davidj@dx-email.com" },
+            new string[] { "Kelly", "Rodriguez", "kellyr@dx-email.com" },
+            new string[] { "Bart", "Arnaz", "barta@dx-email.com" },
+            new string[] { "Jennifer", "Hobbs", "jennyh@dx-email.com" },
+            new string[] { "Stu", "Pizaro", "stu@dx-email.com" },
+            new string[] { "Bartolomeo", "Pizaro", "bartop@dx-email.com" },
+        };
+        public static IApplicationBuilder UseXpoDemoData(this IApplicationBuilder app) {
+            using(var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope()) {
+                UnitOfWork uow = scope.ServiceProvider.GetService<UnitOfWork>();
+                if(!uow.Query<User>().Any()) {
+                    foreach(var row in demoData) {
+                        var newUser = new User(uow) {
+                            FirstName = row[0],
+                            LastName = row[1],
+                            Email = row[2]
+                        };
+                    }
+                    uow.CommitChanges();
+                }
+            }
+            return app;
+        }
+    }
+}
