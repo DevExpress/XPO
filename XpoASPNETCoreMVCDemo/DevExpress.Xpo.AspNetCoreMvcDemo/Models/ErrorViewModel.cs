073dcdd287c77ee427357a814be05a991bfa5c8d
karpenko.maksim <maksim.karpenko@devexpress.com>
2019-04-19 17:38:30 +0300

    Remove old demos

--- XpoASPNETCoreMVCDemo/DevExpress.Xpo.AspNetCoreMvcDemo/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace DevExpress.Xpo.AspNetCoreMvcDemo.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
