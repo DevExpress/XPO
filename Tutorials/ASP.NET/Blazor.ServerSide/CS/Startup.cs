cdc665783d74982265c47e91eda73df4eda9cc86
karpenko.maksim <maksim.karpenko@devexpress.com>
2019-10-25 16:08:27 +0300

    Fix 'Cross-Thread operation detected' exception when editing data

--- Tutorials/ASP.NET/Blazor.ServerSide/CS/Startup.cs
+++ Tutorials/ASP.NET/Blazor.ServerSide/CS/Startup.cs
@@ -31,13 +31,14 @@ namespace BlazorServerSideApplication {
             //services.AddSingleton<WeatherForecastService>();
             services.AddScoped<CustomerService>();
             services.AddScoped<OrderService>();
-            services.AddXpoDefaultUnitOfWork(true, options => options
-                    .UseConnectionString(Configuration.GetConnectionString("ImMemoryDataStore"))
-                    .UseThreadSafeDataLayer(true)
-                    .UseConnectionPool(false) // Remove this line if you use a database server like SQL Server, Oracle, PostgreSql etc.                    
-                    .UseAutoCreationOption(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema) // Remove this line if the database already exists
-                    .UseEntityTypes(typeof(Customer), typeof(Order)) // Pass all of your persistent object types to this method.
+            services.AddXpoDefaultDataLayer(ServiceLifetime.Singleton, dl => dl
+                .UseConnectionString(Configuration.GetConnectionString("ImMemoryDataStore"))
+                .UseThreadSafeDataLayer(true)
+                .UseConnectionPool(false) // Remove this line if you use a database server like SQL Server, Oracle, PostgreSql etc.
+                .UseAutoCreationOption(DevExpress.Xpo.DB.AutoCreateOption.DatabaseAndSchema) // Remove this line if the database already exists
+                .UseEntityTypes(typeof(Customer), typeof(Order)) // Pass all of your persistent object types to this method.
             );
+            services.AddXpoDefaultUnitOfWork();
             //Added lines end.
         }
 
