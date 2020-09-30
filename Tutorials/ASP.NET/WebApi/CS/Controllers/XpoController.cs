a661ad353f2f0c76092b7babcdf8782d03514031
Uriah <uriah@devexpress.com>
2020-09-30 08:58:15 +0300

    added blazor web assembly example

---
+++ Tutorials/ASP.NET/WebApi/CS/Controllers/XpoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+using DevExpress.Xpo.DB;
+
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApplication.Controllers {
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class XpoController : ControllerBase {
+        readonly WebApiDataStoreService DataStoreService;
+        public XpoController(WebApiDataStoreService dataStoreService) {
+            this.DataStoreService = dataStoreService;
+        }
+        [HttpGet]
+        public string Hello() {
+            return "Use WebApiDataStoreClient to connect to this service.";
+        }
+        [HttpPost]
+        [EnableCors("XPO")]
+        public Task<OperationResult<UpdateSchemaResult>> UpdateSchema([FromQuery] bool doNotCreateIfFirstTableNotExist, [FromBody] DBTable[] tables) {
+            return DataStoreService.UpdateSchemaAsync(doNotCreateIfFirstTableNotExist, tables);
+        }
+        [HttpPost]
+        [EnableCors("XPO")]
+        public Task<OperationResult<SelectedData>> SelectData([FromBody] SelectStatement[] selects) {
+            return DataStoreService.SelectDataAsync(selects);
+        }
+        [HttpPost]
+        [EnableCors("XPO")]
+        public Task<OperationResult<ModificationResult>> ModifyData([FromBody] ModificationStatement[] dmlStatements) {
+            return DataStoreService.ModifyDataAsync(dmlStatements);
+        }
+    }
+}
---
+++ Tutorials/ASP.NET/WebApi/CS/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
+
+using WebApiApplication.Helpers;
+
+namespace WebApiApplication {
+    public class Startup {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env) {
+            Configuration = configuration;
+            HostingEnvironment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment HostingEnvironment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+
+
+            // Configure XPO Data Store Service
+            string connectionString;
+            AutoCreateOption autoCreateOption = AutoCreateOption.SchemaAlreadyExists;
+            if(HostingEnvironment.IsDevelopment()) {
+                connectionString = Configuration.GetConnectionString("Dev");
+                autoCreateOption = AutoCreateOption.DatabaseAndSchema;
+            }
+            else {
+                connectionString = Configuration.GetConnectionString("Prod");
+                connectionString = XpoDefault.GetConnectionPoolString(connectionString);
+            }
+            IDataStore dataStore = XpoDefault.GetConnectionProvider(connectionString, autoCreateOption);
+            WebApiDataStoreService service = new WebApiDataStoreService(dataStore);
+            services.AddSingleton(dataStore);
+            services.AddSingleton(service);
+
+            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddCors(options =>
+                options.AddPolicy("XPO", builder =>
+                    builder.WithOrigins("https://localhost:44317")
+                        .WithMethods("POST")
+                        .WithHeaders("Content-Type")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if(env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseCors();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+
+            app.UseXpoDemoData();
+
+        }
+    }
+}
